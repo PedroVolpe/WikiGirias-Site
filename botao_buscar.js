39a1875b4306355dbea15e9875b1919939f91710
Voolpe <volpin2002@gmail.com>
2024-05-14 17:34:11 -0300

    Fiz umas paginas a mais

--- botao_buscar.js
+++ botao_buscar.js
@@ -6,8 +6,23 @@ function realizarPesquisa() {
   if (valorPesquisa.trim() === "foi de base") {
       // Redirecionar para a página de gíria
       window.location.href = "pagGiria.html";
+  } else if (valorPesquisa.trim() === "zueira") {
+    // Redirecionar para a página de gíria
+    window.location.href = "pagGiria2.html";
+  } else if (valorPesquisa.trim() === "papo reto") {
+      // Redirecionar para a página de gíria
+      window.location.href = "pagGiria3.html";
+  } else if (valorPesquisa.trim() === "sacar" || valorPesquisa.trim() === "sacou" ) {
+        // Redirecionar para a página de gíria
+        window.location.href = "pagGiria4.html";
+  } else if (valorPesquisa.trim() === "trampo") {
+        // Redirecionar para a página de gíria
+        window.location.href = "pagGiria5.html";
+  } else if (valorPesquisa.trim() === "paia") {
+        // Redirecionar para a página de gíria
+        window.location.href = "pagGiria6.html";
   } else {
-      // Redirecionar para a página de resultados de pesquisa
-      window.location.href = "pagResultadoPesquisa.html";
-  }
+    // Redirecionar para a página de resultados de pesquisa
+    window.location.href = "pagResultadoPesquisa.html";
+}
 }
